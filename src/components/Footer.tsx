132d91f83db8533c60d1e2927bc19c5c9ed13193
Joao Oliveira <jpsoliveira.me@gmail.com>
2024-08-10 11:23:26 -0300

    feat: add creative commons license

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
-  return <header className="text-sm text-gray-500">Copyright </header>;
+  return (
+    <footer className="w-full flex justify-center text-sm p-4">
+      <Link
+        to={"https://creativecommons.org/licenses/by-nc-sa/4.0/"}
+        className="underline mr-1 opacity-40 transition-opacity duration-200 hover:opacity-100"
+      >
+        {"CC BY-NC-SA 4.0"}
+      </Link>
+      <span className="mr-1 opacity-40">{"2024-PRESENT © João Oliveira"}</span>
+    </footer>
+  );
 }
