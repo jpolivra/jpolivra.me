4d179c74b3854c308d91b1f6df305fcb8e8dfd6b
Joao Oliveira <jpsoliveira.me@gmail.com>
2024-08-13 13:02:06 -0300

    chore: update profile picture

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
-    <footer className="w-full flex justify-center text-sm p-4">
+    <footer className="w-full flex justify-center text-sm px-7">
       <Link
         to={"https://creativecommons.org/licenses/by-nc-sa/4.0/"}
-        className="underline mr-1 opacity-40 transition-opacity duration-200 hover:opacity-100"
+        className="underline mr-1 opacity-40 transition duration-200 hover:opacity-100 hover:text-[#FF8E3C]"
       >
         {"CC BY-NC-SA 4.0"}
       </Link>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,39 @@
+import profile from "@assets/profile.jpeg";
+
 export function Home() {
   return (
-    <div className="flex flex-col gap-4 px-4 mt-10 mb-10 max-w-[400px] text-justify	">
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nesciunt
-        corrupti quam aut sapiente dolorum sequi magni distinctio quos? A,
-        eligendi possimus? Earum minima adipisci omnis distinctio eligendi, rem
-        aliquam?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
-        dolorem architecto magni eum facere deleniti delectus natus itaque
-        autem, maxime consequatur! Ratione sequi ex quis culpa, repellat impedit
-        dolorum accusantium!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-        accusantium obcaecati eos cumque dolorum asperiores accusamus
-        consequatur ullam, perferendis, nemo nesciunt, facilis magni! Aliquam
-        perferendis veritatis dolore expedita. Non, omnis!
-      </p>
-    </div>
+    <main className="py-10 px-7">
+      <h1 className="text-4xl font-extrabold max-w-[650px] mb-8 m-auto">
+        João Oliveira
+      </h1>
+      <article className="max-w-[650px] m-auto flex flex-col gap-5 mb-8">
+        <p>Hi! I'm João Oliveira.</p>
+        <p>Working at Framework Digital</p>
+        <p>
+          I'm a software engineer. I started my career at 2021 and since there
+          I've been building agricultural technology at Syngenta Digital.
+          Currently, I've been spenting my time learning about computer graphics
+          specifically with WebGL, you can find some stuff I make here.
+        </p>
+      </article>
+      <article className="max-w-[650px] m-auto flex flex-col-reverse gap-5 md:flex-row">
+        <aside className="basis-1/2 flex flex-col gap-5">
+          <p>
+            I write blog posts about the things I am building and learning
+            about. Sometimes, I also do some live coding streams on YouTube.
+          </p>
+          <p>Find me on</p>
+          <span>Github</span>
+          <span>YouTube</span>
+        </aside>
+        <div className="basis-1/2 p-1 rounded-lg">
+          <img
+            src={profile}
+            className="rounded-lg m-auto"
+            alt="myself in a monocromatic color palette"
+          />
+        </div>
+      </article>
+    </main>
   );
 }
